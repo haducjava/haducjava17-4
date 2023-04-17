9b21f7d2b4550556b98d4acafa163bfad31f26ae
unknown <haducgame@gmail.com>
2023-04-17 18:51:11 +0700

    bai 1 17/4

---
+++ src/Chuoi.java
@@ -0,0 +1,18 @@
+public class Chuoi {
+    public static void main(String[] args) {
+        String s = "Xin chào bạn đến với java";
+        System.out.println(s);
+        String t = "Cố lên";
+        System.out.println(t);
+        String g = "Gía Trị của G"; // ko dc
+
+        // ghép chuỗi
+        String ghep = s + t;
+        System.out.println(ghep);
+
+        // ghép chuỗi với kiểu dữ liệu int
+        int i = 9;
+        System.out.println(s + i);
+        System.out.println("Gía Trị Của G="+i);
+    }
+}
